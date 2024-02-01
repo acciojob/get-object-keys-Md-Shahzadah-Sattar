4a648b5968a3e77970a04ca34b61053f94d4c6ed
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2024-02-01 17:38:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,19 @@
 //your JS code here. If required.
-function sortObjectsByKey(arr, keyName, compareFunc) {
-  // Do not console.log here
-  // Just return the result
-   return arr.sort((a,b) => {
-       return compareFunc(a[keyName], b[keyName])
-   })
-}
-
-const compareFunc = (a, b) => {
-  if (a < b) {
-    return -1;
-  }
-
-  if (a > b) {
-    return 1;
-  }
-
-  return 0;
+let student = {
+	name : 'Md Shahzadah'
 };
-
-const readline = require("readline");
-
-const input = [];
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.on("line", function (cmd) {
-  input.push(cmd);
-});
-
-rl.on("close", function (cmd) {
-  Main(input);
-});
-
-function Main(input) {
-  const arr = JSON.parse(input[0]);
-  console.log(sortObjectsByKey(arr, input[1], compareFunc));
+Object.prototype.getKeys = function(){
+	return Object.keys(this); 
 }
+let keys = student.getKeys();
+console.log(keys);
+
+// let student = {
+//   name: "John"
+// };
+
+// Object.prototype.getKeys = function() {
+//   return Object.keys(this);
+// };
+
+// console.log(student.getKeys()); // Output: ["name"]
